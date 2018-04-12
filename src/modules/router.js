6d2fc47bcf185d4fb094ea3a9624f0bd7a33b9c7
coocssweb <1974740999@qq.com>
2018-04-12 21:53:40 +0800

    添加路由模块

--- src/modules/router.js
+++ src/modules/router.js
@@ -1,4 +1,47 @@
 /**
- * 
+ * 路由管理
  * Created by 王佳欣 on 2018/4/11.
  */
+import URI from 'urijs';
+
+// 替换Hash
+const replaceHash = (path) => {
+    window.location.replace(`${_uri.origin()}/#${path}`);
+};
+
+
+
+export default class Router {
+    constructor (routes) {
+        this.routes = routes;
+        this.current = {
+            path: location.path || '/',
+            hash: location.hash || '',
+        };
+    }
+    // 解析组件
+    resolveComponents () {
+
+    }
+    match () {
+
+    }
+    // 跳转
+    transitionTo (hash, done) {
+        done && done();
+    }
+    /**
+     * 路由监听
+     */
+    setupListeners () {
+        window.addEventListener('hashchange', () => {
+            let uri = new URI();
+            this.transitionTo(uri.hash(), route => {
+                replaceHash(route.fullPath)
+            })
+        })
+    }
+    init () {
+        this.setupListeners();
+    }
+};
