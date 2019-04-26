3edd9a5f4230b4cea65e601e8a3363934b391b65
coocssweb <1974740999@qq.com>
2019-04-26 19:51:27 +0800

    remove config

--- build/utils.js
+++
@@ -1,5 +0,0 @@
-const path = require('path');
-
-exports.resolve = function resolve (...args) {
-    return path.join(__dirname,  '..', ...args)
-};
--- build/webpack.test.config.js
+++
@@ -1,8 +0,0 @@
-const merge = require('webpack-merge');
-const NODE_ENV = process.env.NODE_ENV = 'test';
-const webpackBaseConfig = require('./webpack.base.config')(NODE_ENV);
-const webpackConfig = merge(webpackBaseConfig, {
-
-});
-
-module.exports = webpackConfig;
